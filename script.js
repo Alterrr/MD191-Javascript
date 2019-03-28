09a397ed492fb3da069317c7c33cafe47458e980
Michael <Michael@kurzmeier.info>
2019-03-28 09:45:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var quiz = [
+var quiz = [ // this array holds the question number, questions and answers. 
   [1, "Question 1", "1"],
   [2, "Question 2", "2"],
   [3, "Question 3", "3"]
@@ -6,15 +6,15 @@ var quiz = [
 
 var answer;
 var response;
-var cor = 0;
+var cor = 0; //counts correct answers
 
 for(var i = 0; i < (quiz.length+1); i += 1){
-//document.write(i);
-if (i===quiz.length){
+
+if (i===quiz.length){ //if the quiz is in its last loop, show the number of correct answers
   document.write(`<h2>You got ${cor} out of ${i} questions right</h2>`);
-}else {
+}else { //in all other cases, continue with the questions
   answer = prompt(quiz[i][1]);
-  response = answer.toLowerCase();
+  response = answer.toLowerCase(); //this converts all answers to lower case, so that "Answer" and "answer" count
     
   if (response === quiz[i][2]){
       cor +=1;
