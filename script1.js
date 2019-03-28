ce7a70342c5e4dc42403911c90eaf4cd56f3be24
Michael <Michael@kurzmeier.info>
2019-03-28 16:19:15 +0000

    Update script1.js
    version with a start button

--- script1.js
+++ script1.js
@@ -1,37 +1,35 @@
+document.getElementById("Start").onclick = function() {runQuiz()};
 
-var quiz = [
-  [1, "Question 1", "1"],
-  [2, "Question 2", "2"],
-  [3, "Question 3", "3"]
-];
-
-var answer;
-var response;
-var cor = 0;
-
+function test(){
+document.write("test");
+}
 function runQuiz() {
-for(var i = 0; i < (quiz.length+1); i += 1){
-//document.write(i);
-if (i===quiz.length){
-  document.write(`<h2>You got ${cor} out of ${i} questions right</h2>`);
-}else {
-  answer = prompt(quiz[i][1]);
-  response = answer.toLowerCase();
-    
-  if (response === quiz[i][2]){
-      cor +=1;
-      document.getElementById("p1").innerHTML = `<h2>You got question ${quiz[i][0]} right</h2>`;
-      //document.write();
-  }else {
-      document.write(`<h2>You got question ${quiz[i][0]} wrong</h2>`);
+  var quiz = [
+    [1, "Question 1", "1"],
+    [2, "Question 2", "2"],
+    [3, "Question 3", "3"]
+  ];
+
+  var answer;
+  var response;
+  var cor = 0;
+
+
+  for (var i = 0; i < (quiz.length + 1); i += 1) {
+    //document.write(i);
+    if (i === quiz.length) {
+      document.write(`<h2>You got ${cor} out of ${i} questions right</h2>`);
+    } else {
+      answer = prompt(quiz[i][1]);
+      response = answer.toLowerCase();
+
+      if (response === quiz[i][2]) {
+        cor += 1;
+    //document.getElementById("output").innerHTML = `<h2>You got question ${quiz[i][0]} right</h2>`;
+        document.write(`<h2>You got question ${quiz[i][0]} right</h2>`);
+      } else {
+        document.write(`<h2>You got question ${quiz[i][0]} wrong</h2>`);
+      }
+    }
   }
 }
-}
-return;}
-
-
-
-
-
-
-
