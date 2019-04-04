f31424fa6d794c83a1ce8310ba6892312082e94e
Michael <Michael@kurzmeier.info>
2019-04-04 15:48:57 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ document.getElementById("Start").onclick = function() {runQuiz()};
 
 function runQuiz() {
   var quiz = [
-    [1, "Question 1", "1"],
+    [1, "Question 1", "1"], //answers must be all lowercase
     [2, "Question 2", "2"],
     [3, "Question 3", "3"]
   ];
@@ -12,21 +12,17 @@ function runQuiz() {
   var cor = 0;
 
 
-  for (var i = 0; i < (quiz.length + 1); i += 1) {
-    //document.write(i);
+  for (var i = 0; i < (quiz.length + 1); i += 1) { //go through quiz
     if (i === quiz.length) {
-      //document.write(`<h2>You got ${cor} out of ${i} questions right</h2>`);
-      document.getElementById("output").innerHTML = (`<h2>You got ${cor} out of ${i} questions right</h2>`)
+      document.getElementById("output").innerHTML = (`<h2>You got ${cor} out of ${i} questions right</h2>`) //print evaluation after the last question
     } else {
       answer = prompt(quiz[i][1]);
       response = answer.toLowerCase();
 
       if (response === quiz[i][2]) {
         cor += 1;
-        //document.write(`<h2>You got question ${quiz[i][0]} right</h2>`);
         document.getElementById("output").innerHTML = `<h2>You got question ${quiz[i][0]} right</h2>`;
       } else {
-        //document.write(`<h2>You got question ${quiz[i][0]} wrong</h2>`);
         document.getElementById("output").innerHTML = `<h2>You got question ${quiz[i][0]} wrong</h2>`
       }
     }
